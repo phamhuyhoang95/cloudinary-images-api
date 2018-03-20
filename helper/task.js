e1d2d8002786546a941efd2d2a5b503cf107c702
phamhuyhoang <phamhuyhoang995@gmail.com>
2018-03-20 21:27:03 +0700

    backup data with cloudinary

--- helper/task.js
+++ helper/task.js
@@ -1,8 +1,18 @@
 var CronJob = require('cron').CronJob;
 const fs = require('fs')
 const moment = require('moment')
-console.log("Job is running")
-var job = new CronJob('*/1 * * * *', function () {
+const cloudinary = require('cloudinary')
+require('dotenv').config({
+    path: '.localenv',
+    silent: true
+})
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+var job = new CronJob(`0 0 */${process.env.BACKUP_TIME} * * *`, function () {
+        console.log("Job is started")
         // do something
         var http = require("http");
         const dates = moment().format('YYYY-MM-DD hh:mm:ss')
@@ -23,19 +33,29 @@ var job = new CronJob('*/1 * * * *', function () {
             res.on("data", function (chunk) {
                 chunks.push(chunk);
             });
-
             res.on("end", function () {
-                var body = Buffer.concat(chunks);
-                fs.writeFileSync(`${__dirname}/backup/data-${dates}.json`, body.toString())
+                var buffer = Buffer.concat(chunks);
+                cloudinary.v2.uploader.upload_stream({
+                        public_id: `data-${dates}.json`,
+                        resource_type: 'auto',
+                        folder: 'backup'
+                    }, (error, result) => {
+                        if (error) {
+                            console.log(error)
+                        }
+                        console.log("Job done!")
+                    })
+                    .end(buffer);
             });
         });
-
         req.end();
     }, function () {
         // get backup data from API
         console.log("Job is stoped !")
         /* This function is executed when the job stops */
     },
-    true, /* Start the job right now */
+    false, /* Start the job right now */
     // timeZone /* Time zone of this job. */
 );
+console.log("prepare for start")
+job.start()
